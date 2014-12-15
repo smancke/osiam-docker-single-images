0c6cee3e2b793f85161a3bc4a9868fdf9a938b66
smancke <s.mancke@tarent.de>
2014-12-15 23:19:16 +0100

    initial create of osiam docker images

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+. build.conf
+
+for IMAGE in common auth-server resource-server; do #  addon-administration addon-self-administration; do
+    echo 
+    echo "-----------------------------------------------"
+    echo "-- building: $IMAGE"
+    echo "-----------------------------------------------"
+
+    export IMAGE
+
+    mkdir -p build/$IMAGE
+    test -e build/$IMAGE/osiam-${IMAGE}_${OSIAM_VERSION}_all.deb \
+        || curl -o build/$IMAGE/osiam-${IMAGE}_${OSIAM_VERSION}_all.deb -L https://github.com/osiam/distribution/releases/download/v${OSIAM_VERSION}/osiam-${IMAGE}_${OSIAM_VERSION}_all.deb \
+        || exit 1
+    cat $IMAGE/Dockerfile.in | envsubst > build/$IMAGE/Dockerfile
+    
+
+    test -e $IMAGE/conf && cp -r $IMAGE/conf build/$IMAGE/
+    test -e $IMAGE/conf/${IMAGE}.properties \
+        && cat $IMAGE/conf/${IMAGE}.properties | envsubst > build/$IMAGE/conf/${IMAGE}.properties
+
+    docker build --tag $REGISTRY/osiam-$IMAGE build/$IMAGE
+    docker build --tag $REGISTRY/osiam-$IMAGE:$OSIAM_VERSION build/$IMAGE
+done
+
