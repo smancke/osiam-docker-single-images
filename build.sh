5f91ec9baaed1b90bf939203f919753daa72b254
smancke <s.mancke@tarent.de>
2014-12-23 00:29:23 +0100

    fixed some variables

--- build.sh
+++ build.sh
@@ -20,7 +20,13 @@ for IMAGE in common auth-server resource-server addon-administration addon-self-
     cat Dockerfile-$IMAGE.in | envsubst > build/$IMAGE/Dockerfile
 
     # append all 'OSIAM_' shell variables als ENV defaults in the Dockerfile
-    set | grep OSIAM_ | sed 's/^/ENV /;s/=/ /' >> build/$IMAGE/Dockerfile
+
+    set | grep OSIAM_ | sed 's/\(.*\)=\(.*\)/ENV \1 \2/' >> build/$IMAGE/Dockerfile
+
+    ## if multiline envs work in docker1.4, use this code:
+    #echo "ENV \\" >> build/$IMAGE/Dockerfile
+    #set | grep OSIAM_ | sed 's/\(.*\)=\(.*\)/  \1="\2\" \\/' >> build/$IMAGE/Dockerfile
+    #echo "  dummy=dummy" >> build/$IMAGE/Dockerfile # only a fix for last \
     
     # replace script to 
     #   replace the values of configurable properties with shell variables
@@ -40,4 +46,4 @@ echo "-----------------------------------------------"
 mkdir -p build/postgresql
 cat postgresql/Dockerfile.in | envsubst > build/postgresql/Dockerfile
 cp -r postgresql/conf build/postgresql/
-docker build --tag postgresql/osiam-postgresql:$OSIAM_VERSION build/postgresql
+docker build --tag $REGISTRY/osiam-postgres:$OSIAM_VERSION build/postgresql
